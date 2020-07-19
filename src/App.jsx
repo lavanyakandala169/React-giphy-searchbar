b1a593970e2549f8cda199aad85354d40b46a780
lavanya <lavanyamk2019@gmail.com>
2020-07-20 01:44:23 +0530

    Gif

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import ReactDom from 'react-dom';
+import Giphy from './components/Giphy';
+
+
+const App = () => {
+  return <div>
+  <Giphy/>
+  </div>
+  
+}
+
+
+export default App;
